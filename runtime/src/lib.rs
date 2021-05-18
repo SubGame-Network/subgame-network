b60d48086c69599c2fd08d53336d4993bd911f0b
xtony77 <xtony77@gmail.com>
2021-05-18 16:17:03 +0800

    feat: update version 110
    - edit chain_spec env setup.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -108,7 +108,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subgame"),
     impl_name: create_runtime_str!("subgame"),
     authoring_version: 1,
-    spec_version: 109,
+    spec_version: 110,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,

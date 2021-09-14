fe6f6ee198a15919524164b012db1afdea902901
ss910316 <ss910316@gmail.com>
2021-09-14 09:51:15 +0800

    update dev runtime version 125

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -140,7 +140,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("subgame"),
     impl_name: create_runtime_str!("subgame"),
     authoring_version: 1,
-    spec_version: 124,
+    spec_version: 125,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
